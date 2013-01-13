c1f1ea9bead716c84bd568b629b629cf94a8e61e
Brian Le <blsymph@gmail.com>
2013-01-13 18:19:18 -0500

    Update database configuration

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,11 +10,16 @@ describe "StaticPages" do
 			visit '/static_pages/home'
 			page.should have_selector('h1', :text => 'Sample App')
 		end
-		
-		it "should have the title 'Home'" do
-			visit '/static_pages/home'
-			page.should have_selector('title', :text => "#{base_title} | Home")
-		end
+
+        it "should have the base title" do
+            visit '/static_pages/home'
+            page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+        end
+
+        it "should not have a custom page title" do
+            visit '/static_pages/home'
+            page.should_not have_selector('title', :text => '| Home')
+        end
 	end
 
 	describe "Help page" do
