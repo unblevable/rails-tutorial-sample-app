da5a0611abda460d73e33a580e71e8fc7bff379e
Brian Le <blsymph@gmail.com>
2013-01-17 15:12:33 -0500

    Finish sign in

--- Gemfile
+++ Gemfile
@@ -38,4 +38,6 @@ group :test do
 	gem 'rb-inotify', '0.8.8'
 	gem 'libnotify', '0.5.9'
     gem 'factory_girl_rails', '4.1.0'
+    gem 'cucumber-rails', '1.2.1', :require  => false
+    gem 'database_cleaner', '0.7.0'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,7 @@ describe User do
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
     it { should respond_to(:authenticate) }
+    it { should respond_to(:remember_token) }
 
     it { should be_valid }
 
@@ -105,5 +106,10 @@ describe User do
     describe "with a password that's too short" do
         before { @user.password = @user.password_confirmation = "a" * 5 }
         it { should be_invalid }
+    end
+
+    describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
     end
 end
