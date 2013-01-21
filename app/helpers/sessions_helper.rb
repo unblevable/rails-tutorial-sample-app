36654b8b9a497863f6ef82f1459b1926a6cdc12e
Brian Le <blsymph@gmail.com>
2013-01-20 22:29:46 -0500

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,6 +21,13 @@ module SessionsHelper
     user == current_user
   end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
   def signed_in?
     !current_user.nil?
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@
 class User < ActiveRecord::Base
     attr_accessible :email, :name, :password, :password_confirmation
     has_secure_password
+    has_many :microposts, dependent: :destroy
 
     before_save { |user| user.email = email.downcase }
     before_save :create_remember_token
@@ -22,6 +23,11 @@ class User < ActiveRecord::Base
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 6 }
     validates :password_confirmation, presence: true
+
+    def feed
+      # This is preliminary. See "Following users" for the full implementation  .
+      Micropost.where("user_id = ?", id)
+    end
 
     private
 
